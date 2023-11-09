c0bfafe1f6730861623e4c3191585a1c5a66a373
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-09 20:47:46 -0300

    att

--- projeto/urls.py
+++ projeto/urls.py
@@ -4,6 +4,7 @@ URL configuration for projeto project.
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
+
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
@@ -21,5 +22,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
 ]
