3b39bc2298005844c71d67e8463999c69d9e89ff
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-09 21:39:32 -0300

    att

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,27 +1,16 @@
-"""
-URL configuration for projeto project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# Porta de entrada da nossa aplicação, onde ficarão todas os caminhos do projeto
-
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse as res # Aqui chamo uma função genéria para retornar, porque toda view prencisa gerar um Request e retornar um responde
+# função temporária:
+
+
+#HTTP Request 
+def my_view(request):
+    return res('o inicio de um sonho')
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
+    path('', my_view),
 ]
+
