dd429578d38ee2ab4db823ccdd7164ecbc89cf87
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-18 09:24:14 -0300

    att

--- projeto/settings.py
+++ projeto/settings.py
@@ -85,8 +85,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -104,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
