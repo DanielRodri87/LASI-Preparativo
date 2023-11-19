627976d26dda3949faecfd808908acc4fbcb6c70
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-19 17:02:24 -0300

    scrollbar

--- recipes/static/recipes/script/scroolbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if ('scrollbarColor' in document.documentElement.style) {
-        document.documentElement.style.scrollbarColor = 'black black';
-    } else {
-        console.log('seu navegador é uma merda.');
-    }
-});
-
