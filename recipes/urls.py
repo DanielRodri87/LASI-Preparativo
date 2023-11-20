a7a00e0cf1d4862f7af473ef4dd93605209518b3
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-19 22:45:27 -0300

    att

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from recipes.views import home
-
+from . import views # da pasta que estou importo views
 urlpatterns = [
-    path('', home), # Home    path('sobre/', sobre), # /sobre
-
+    path('', views.home), # Home    path('sobre/', sobre), # /sobre
+    path('recipes/<int:id>/', views.recipe),
 ]
+
--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,8 @@ def home(request):
        'name': 'Daniel',
     })
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+       'name': 'Daniel',
+    })
+
