7d43e3c8bac8f32e966c3818ef54ae166ebbb2fe
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-11-20 07:59:19 -0300

    herança

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse as res
 
-
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
        'name': 'Daniel',
     })
-
+    
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
        'name': 'Daniel',
     })
-
