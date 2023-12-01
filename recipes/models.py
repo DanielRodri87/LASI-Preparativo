82d96eb451f222faf66b9916fa8923973886058f
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-12-01 17:21:47 -0300

    configurando banco de dados

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -14,17 +19,6 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    
-    
-    
-# EDITED
-# title description slug
-# preparation_time preparation_time_unit
-# servings servings_unit
-# preparation_step
-# preparation_step_is_html
-# created_at updated_at
-# is_published
-# cover
-# category (Relação)
-# Author (Relação)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
