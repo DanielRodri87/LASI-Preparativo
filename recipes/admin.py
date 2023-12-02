279d78025efccda04cb7ff10c38f1e3d56eb1130
DanielRodri87 <daniel.acaua.piaui.brasil@gmail.com>
2023-12-02 09:52:06 -0300

    configurando banco de dados

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
-# Registra seus modelos aqui
-# Área Administrativa para um CRUD
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+ 
+@admin.register(Recipe)
+       
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+    
+
+admin.site.register(Category, CategoryAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
     
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -13,7 +15,7 @@ class Recipe(models.Model):
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
-    preparation_steps = models.TimeField()
+    preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,3 +24,5 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
+    
+    
